6a929f78bc6c6a7867d86b428c09972489ca9b4a
Rama-ab <ramaabdo657@gmail.com>
2026-09-17 16:10:13 -0700

    Setting up the Laravel modules package

--- config/auth.php
+++ config/auth.php
@@ -64,7 +64,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', User::class),
+            //'model' => env('AUTH_MODEL', User::class),
+             'model' => Modules\Core\app\Models\User::class, 
         ],
 
         // 'users' => [
